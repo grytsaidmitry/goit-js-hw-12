cfa0d220f68a2baac34c5682e0a4e64fb51a94a7
grytsaidmitry <dimasicus2008@gmail.com>
2026-02-22 12:17:09 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,149 @@
+import './css/styles.css';
+import './css/loader.css';
+import arrowLeft from './img/arrow.svg';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import getImagesByQuery from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+const btnLoading = document.querySelector('.btn-loading');
+const gallery = document.querySelector('.gallery');
+
+let page = 1;
+let value = '';
+let totalHits = 0;
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  clearGallery();
+  hideLoadMoreButton();
+  page = 1;
+
+  const query = event.target.elements['search-text'].value.trim();
+  value = query;
+
+  if (!query) {
+    hideLoader();
+    iziToast.show({
+      class: 'custom-error-toast',
+      message: 'Enter the search word',
+      position: 'topRight',
+      timeout: 5000,
+    });
+    return;
+  }
+
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query, page);
+
+    if (!data.hits || data.hits.length === 0) {
+      iziToast.show({
+        class: 'custom-error-toast',
+        message:
+          'Sorry, there are no images matching your search query. Please, try again!',
+        position: 'topRight',
+        layout: 2,
+        timeout: 5000,
+        close: true,
+        closeOnEscape: true,
+        transitionIn: 'fadeInLeft',
+        transitionOut: 'fadeOut',
+        iconUrl: arrowLeft,
+        iconColor: '#ffffff',
+      });
+      return;
+    }
+
+    totalHits = data.totalHits;
+    createGallery(data.hits);
+    page += 1;
+
+    if ((page - 1) * 15 < totalHits) {
+      showLoadMoreButton();
+    } else {
+      hideLoadMoreButton();
+      iziToast.show({
+        class: 'custom-error-toast',
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+        timeout: 5000,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+
+    iziToast.show({
+      class: 'custom-error-toast',
+      message: 'Something went wrong while fetching data',
+      position: 'topRight',
+      timeout: 5000,
+    });
+  } finally {
+    hideLoader();
+  }
+});
+
+btnLoading.addEventListener('click', async () => {
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(value, page);
+
+    if (data.hits && data.hits.length > 0) {
+      createGallery(data.hits);
+
+      const firstCard = gallery.querySelector('.gallery-container');
+      if (firstCard) {
+        const { height: cardHeight } = firstCard.getBoundingClientRect();
+        window.scrollBy({
+          top: cardHeight * 2,
+          behavior: 'smooth',
+        });
+      }
+
+      page += 1;
+
+      // Перевірка, чи користувач дійшов до кінця
+      if ((page - 1) * 15 >= totalHits) {
+        hideLoadMoreButton();
+        iziToast.show({
+          class: 'custom-error-toast',
+          message: "We're sorry, but you've reached the end of search results.",
+          position: 'topRight',
+          timeout: 5000,
+        });
+      }
+    } else {
+      hideLoadMoreButton();
+      iziToast.show({
+        class: 'custom-error-toast',
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+        timeout: 5000,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    iziToast.show({
+      class: 'custom-error-toast',
+      message: 'Something went wrong while fetching data',
+      position: 'topRight',
+      timeout: 5000,
+    });
+  } finally {
+    hideLoader();
+  }
+});
